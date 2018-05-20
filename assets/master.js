3ad80d88b77397141801c81aca350f5856122776
spidercrew <baimek88@gmail.com>
2018-05-21 01:43:14 +0700

    Update master.js

--- assets/master.js
+++ assets/master.js
@@ -14,26 +14,4 @@ if((eventbutton==2)||(eventbutton==3)) return false;}
 document.oncontextmenu = mischandler;
 document.onmousedown = mousehandler;
 document.onmouseup = mousehandler;
-//
-$(function(){
- 
-    $(document).on( 'scroll', function(){
- 
-        if ($(window).scrollTop() > 100) {
-            $('.smoothscroll-top').addClass('show');
-        } else {
-            $('.smoothscroll-top').removeClass('show');
-        }
-    });
- 
-    $('.smoothscroll-top').on('click', scrollToTop);
-});
- 
-function scrollToTop() {
-    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-    element = $('body');
-    offset = element.offset();
-    offsetTop = offset.top;
-    $('html, body').animate({scrollTop: offsetTop}, 600, 'linear');
-}
 //]]>
